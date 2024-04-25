f306e6d369742ce6e61b565110a75af73e230578
alumno <alumno@eim-alu-83019.lab.unavarra.es>
2024-04-25 11:09:17 +0200

    Dealer y Hnadler

--- src/main/java/FinalPack/Dealer.java
+++ src/main/java/FinalPack/Dealer.java
@@ -22,4 +22,62 @@ public class Dealer {
     private ExecutorService executor; // will run players
     private ServerSocket server; // server socket
     private Handler_Server[] sockServer; // Array de objetos para hacerlos hilos
+    
+    
+    
+    
+    
+    private void DealerGo() {
+            dcartas = new Juego(dcarta1, dcarta2);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                System.err.println("Capturada InterruptedException: " + e.getMessage());
+                e.printStackTrace(System.err);
+                System.exit(1);
+            }
+            // Hacemos que el dealer se plante si tiene menos de 16 (se podría cambiar a lo que queramos)
+            if (dcartas.getPuntuacion() < 16) {
+                while (dcartas.getPuntuacion() < 16) {
+                    String carta1 = nuevaBaraja.repartirCarta();
+                    dcartas.pedirCarta(carta1);
+                    displayMessage("El dealer golpea..." + carta1 + "\n" + "Total:" + dcartas.getPuntuacion() + "\n");
+                // El dealer coge otra carta y mostramos lo que le ha tocado
+                }
+            }
+            if (dcartas.comprobarSobrepasada()) {
+                displayMessage("¡El dealer se ha sobrepasado!");
+            } else {
+                displayMessage("Puntuación del dealer " + dcartas.getPuntuacion());
+            }
+
+            resultados();
+            // Mostramos los resultados con la función que hemos creado
+        }
+
+        private void tomarCarta() {
+
+            String nextc = nuevaBaraja.repartirCarta();
+            sendData(nextc);
+            jugadores.get(this.conexionID - 1).pedirCarta(nextc);
+            sendData("Tu puntuación: " + jugadores.get(this.conexionID - 1).getPuntuacion());
+            // Si un jugador se sobrepasa, se lo informamos y lo quitamos de los jugadores restantes
+            if (jugadores.get(this.conexionID - 1).comprobarSobrepasada()) {			
+                sendData("¡Sobrepasado!\n");
+                jugadoresRestantes--;
+                if (jugadoresRestantes == 0) {
+                    DealerGo();
+                }
+            }
+
+        }
+
+        private void ComprobarFinalizado() {
+
+            if (jugadoresRestantes == 0) {
+
+                DealerGo();
+            }
+        }
+    
 }
