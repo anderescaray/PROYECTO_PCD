e2ad64d8e91af03359a61ab88a76d1876d9c4193
alumno <alumno@eim-alu-83019.lab.unavarra.es>
2024-04-25 11:52:29 +0200

    Dealer y Hnadler

--- src/main/java/FinalPack/Servidor.java
+++ src/main/java/FinalPack/Servidor.java
@@ -15,22 +15,29 @@ import java.util.concurrent.Executors;
  */
 public class Servidor {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
+
+        Baraja baraja = new Baraja();
+
         System.out.println("El jugador se ha conectado");
         ExecutorService pool = Executors.newFixedThreadPool(500);
         try (ServerSocket listener = new ServerSocket(59002)) {
+
+            Carta carta1 = baraja.sacarCarta();
+            Carta carta2 = baraja.sacarCarta();
+            Carta carta3 = baraja.sacarCarta();
+            Carta carta4 = baraja.sacarCarta();
+// aqui se pone las dos cartas a cada uno con un for del array de jugadores y en handler cada uno elije si se planta o golpea
+            Juego juegojug1 = new Juego(carta1, carta2);
+            Juego juegojug2 = new Juego(carta3, carta4);
             while (true) {
                 pool.execute(new Handler(listener.accept()));
             }
-        }catch(IOException e){
+        } catch (IOException e) {
             System.err.println(e.getMessage());
             e.printStackTrace(System.err);
             System.exit(1);
         }
     }
-    
-    
+
 }
