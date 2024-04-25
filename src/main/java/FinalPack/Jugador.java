fab8e661cd6c4345228cf9bbf5aed21cca0621f7
alumno <alumno@eim-alu-83014.lab.unavarra.es>
2024-04-25 11:44:41 +0200

    Jugador

--- src/main/java/FinalPack/Jugador.java
+++ src/main/java/FinalPack/Jugador.java
@@ -4,6 +4,14 @@
  */
 package FinalPack;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInput;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -12,12 +20,23 @@ import java.util.concurrent.Executors;
  * @author alumno
  */
 public class Jugador {
-
+    private static final String SERVER_ADRESS = "127.0.0.1";
+    private static final int SERVER_PORT = 59002;
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+       try {
+            Socket socket = new Socket(SERVER_ADRESS, SERVER_PORT);
+            ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+            ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream()) ;
+            BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
+            
+    }catch(IOException e){
+        System.err.println(e.getMessage());
+            e.printStackTrace(System.err);
+            System.exit(1);
     }
     
+}
 }
