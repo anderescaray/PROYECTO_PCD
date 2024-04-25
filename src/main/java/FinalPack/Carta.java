8c6faf800e76bda3995afc28f0c1618611ee0113
alumno <alumno@eim-alu-83023.lab.unavarra.es>
2024-04-25 10:46:31 +0200

    otro

--- src/main/java/FinalPack/Carta.java
+++ src/main/java/FinalPack/Carta.java
@@ -4,10 +4,41 @@
  */
 package FinalPack;
 
+import java.io.Serializable;
+
 /**
  *
  * @author alumno
  */
-public class Carta {
+public class Carta implements Serializable {
+    private EnumPalo palo;
+    private String valor;
+
+    public Carta(EnumPalo palo, String valor) {
+        this.palo = palo;
+        this.valor = valor;
+    }
+
+    public EnumPalo getPalo() {
+        return palo;
+    }
+
+    public void setPalo(EnumPalo palo) {
+        this.palo = palo;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return valor + " de " + palo;
+    }
+    
     
 }
